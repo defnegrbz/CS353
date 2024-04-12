04f2cd8da45b4a879c4c0966065d1024c12268aa
Zehra21 <91343907+Zehra21@users.noreply.github.com>
2024-04-12 21:47:37 +0300

    Create WorkoutRepository.java

---
+++ backend/src/test/java/com/example/fitness/repositories/WorkoutRepository.java
@@ -0,0 +1,33 @@
+package com.example.fitness.repositories;
+
+import com.example.fitness.components.Workout;
+
+import jakarta.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface WorkoutRepository extends JpaRepository<Workout, Long> {
+
+    @Query(value = "SELECT * FROM workout w WHERE w.workoutID = ?1", nativeQuery = true)
+    Optional<Workout> findWorkoutByID(Long id);
+
+    @Query(value = "SELECT * FROM workout w WHERE w.workoutID = ?1", nativeQuery = true)
+    Optional<Workout> findWorkoutByTitle(String title);
+
+    @Query(value = "SELECT * FROM workout w WHERE w.trainerID = ?1", nativeQuery = true)
+    Optional<Workout> findWorkoutByTrainer(Long tid);
+
+    @Query(value = "SELECT * FROM workout w", nativeQuery = true)
+    List<Workout> findAll();
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM workout w WHERE w.workoutID = ?1", nativeQuery = true)
+    void deleteById(Long workoutID);
+}
