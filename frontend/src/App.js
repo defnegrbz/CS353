768471000ac943f5f69211051ab228c1c1692162
Zehra21 <91343907+Zehra21@users.noreply.github.com>
2024-04-30 09:38:39 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './components/auth/LoginPage';
+import WorkoutPage from './components/workout/WorkoutsPage';
+import WorkoutCreatePage from './components/workout/WorkoutCreatePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/api/login" element={<LoginPage />} />
+        <Route path="/api/workouts" element={<WorkoutPage />} />
+        <Route path="/api/createWorkout" element={<WorkoutCreatePage />} />
+      </Routes>
+    </Router>
   );
 }
 
