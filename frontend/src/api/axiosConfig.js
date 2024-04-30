7149e03eccd916691b82b6ececc6b92c6ecb7233
Zehra21 <91343907+Zehra21@users.noreply.github.com>
2024-04-30 09:37:06 +0300

    Update axiosConfig.js

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,1 +1,19 @@
+import axios from 'axios';
 
+export const baseURL = 'http://localhost:8080';
+
+const api = axios.create({
+    baseURL: baseURL
+});
+
+//Workout info
+
+export const getWorkout = (id) => (
+    axios.get(`${baseURL}/workout/${id}`)
+);
+
+export const getWorkouts = () => (
+    axios.get(`${baseURL}/workouts`)
+);
+
+export default api
