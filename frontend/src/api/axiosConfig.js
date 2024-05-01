a35c154b9afa0ae8cdf298c121bd7fb5fe3ccc89
Zehra21 <91343907+Zehra21@users.noreply.github.com>
2024-05-01 14:17:27 +0300

    Update axiosConfig.js

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -16,4 +16,34 @@ export const getWorkouts = () => (
     axios.get(`${baseURL}/workouts`)
 );
 
+// Add the addWorkout function
+export const addWorkout = (trainerID, workoutData) => (
+    api.put(`${baseURL}/workouts/${trainerID}/createWorkout`, workoutData)
+);
+
+// Function to filter workouts based on type
+export const filterWorkoutsByType = (type) => (
+    api.get(`/workouts?type=${type}`)
+);
+
+// Function to filter workouts based on calories per unit time
+export const filterWorkoutsByCalories = (minCalories, maxCalories) => (
+    api.get(`/workouts?minCalories=${minCalories}&maxCalories=${maxCalories}`)
+);
+
+// Function to filter workouts based on equipments
+export const filterWorkoutsByEquipments = (equipments) => (
+    api.get(`/workouts?equipments=${equipments}`)
+);
+
+// Function to filter workouts based on target audience
+export const filterWorkoutsByTargetAudience = (targetAudience) => (
+    api.get(`/workouts?targetAudience=${targetAudience}`)
+);
+
+// Function to filter workouts based on intensity level
+export const filterWorkoutsByIntensityLevel = (minIntensity, maxIntensity) => (
+    api.get(`/workouts?minIntensity=${minIntensity}&maxIntensity=${maxIntensity}`)
+);
+
 export default api
