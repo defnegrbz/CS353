2d3bdbc912e287ef2a7660f352e5746e880a8858
rupymeister <anil.altuncu@ug.bilkent.edu.tr>
2024-05-19 17:10:27 +0300

    cors

--- backend/src/main/java/com/example/fitness/controllers/LoginController.java
+++ backend/src/main/java/com/example/fitness/controllers/LoginController.java
@@ -2,6 +2,7 @@ package com.example.fitness.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin(origins = "http://localhost:3000")
 public class LoginController {
     private final LoginService loginService;
      private static final Logger logger = LoggerFactory.getLogger(LoginService.class);
